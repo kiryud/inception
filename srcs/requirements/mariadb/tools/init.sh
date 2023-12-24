7cd875aa2cc824187ecbe79710d00220ca80d942
jijeong <jijeong@student.42seoul.kr>
2023-12-24 17:58:04 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,13 +1,7 @@
 #! /bin/sh
 
-# rc pid 실행을 수행한다.
-rc default
-
 # datadir에 기본 셋업을 진행한다.
 /etc/init.d/mariadb setup
-
-# SQL 쿼리문을 넣기위해 잠시 실행한다.
-rc-service mariadb start
 
 service mysql start;
 
@@ -20,5 +14,3 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';"
 
 mysql -e "FLUSH PRIVILEGES;"
-
-rc-service mariadb stop
