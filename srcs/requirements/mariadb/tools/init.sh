ea01a3954bfbcd4a497605f40ab4fc6851b5875b
jijeong <jijeong@student.42seoul.kr>
2023-12-25 21:14:50 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,12 +6,11 @@ rc default
 
 rc-service mariadb start
 
-echo "mysql -uroot -e 'CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;'"
-echo "mysql -uroot -e 'CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';'"
-echo "mysql -uroot -e 'GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';'"
-echo "mysql -uroot -e 'ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';'"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';"
 
 
 rc-service mariadb stop
 
-/usr/bin/mysqld_safe
