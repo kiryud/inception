e6d2e23ac1bb21c4e38d4a3b85551bec7ab2bca7
jijeong <jijeong@student.42seoul.kr>
2023-12-25 20:41:23 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,14 +6,14 @@ rc default
 
 rc-service mariadb start
 
-cat << EOF | mysql -uroot
-CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;
-CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';
-GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';
-FLUSH PRIVILEGES;
+cat << EOF > init.sql
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';"
 EOF
 
+cat init.sql
 
-rc-service mariadb start
+rc-service mariadb stop
 
