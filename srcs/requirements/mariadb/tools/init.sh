d445c704a669284efbd48d62f9d541a4776f8194
jijeong <jijeong@student.42seoul.kr>
2023-12-25 13:22:57 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
+
 rc default
+
 /etc/init.d/mariadb setup
+
 rc-service mariadb start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASS}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE_NAME};"
+
+mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}@'localhost' IDENTIFIED BY '${MARIADB_PASS}';"
+
 mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASS}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';"
-mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}'; FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$MARIADB_ADMIN_PASS shutdown
 
 rc-service mariadb stop
