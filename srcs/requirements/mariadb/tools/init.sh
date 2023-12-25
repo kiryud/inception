e911cbd69da8b295a43d24a5da8e11ccc7be72ea
jijeong <jijeong@student.42seoul.kr>
2023-12-25 22:53:52 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,13 +1,40 @@
 #! /bin/sh
 
-/etc/init.d/mariadb setup
+# Check if the /run/mysqld directory exists, and create it if not
+if [ ! -d "/run/mysqld" ]
+then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-cat << EOF > init.sql
-CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE_NAME};
-CREATE USER IF NOT EXISTS ${MARIADB_USER}@localhost IDENTIFIED BY '${MARIADB_PASS}';
-GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE_NAME}.* TO ${MARIADB_USER}@% IDENTIFIED BY '${MARIADB_PASS}';
-ALTER USER root@localhost IDENTIFIED BY '${MARIADB_ADMIN_PASS}';
+# Check if the /var/lib/mysql/mysql directory exists, and create it if not
+if [ ! -d "/var/lib/mysql/mysql" ]
+then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	# Create a temporary file to execute SQL commands
+	tmp=`mktemp`
+
+	if [ ! -f "$tmp" ]
+	then
+		return 1
+	fi
+
+	# Define SQL commands to set up MariaDB user and database
+	cat << EOF > $tmp
+USE mysql;
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ADMIN_PASS';
+CREATE DATABASE $MARIADB_DATABASE_NAME;
+CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASS';
+GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mariadbd -u root --bootstrap < init.sql
+	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
+	rm -f $tmp
+fi
+
+exec /usr/bin/mysqld --user=mysql --console
