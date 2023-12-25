880c3436866c4a83b4511a34601d7c5adcba4b45
jijeong <jijeong@student.42seoul.kr>
2023-12-25 20:51:02 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,14 +6,11 @@ rc default
 
 rc-service mariadb start
 
-cat << EOF > init.sql
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;"
-mysql -uroot -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';"
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';"
-EOF
+echo "mysql -uroot -e 'CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME;'"
+echo "mysql -uroot -e 'CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASS}';'"
+echo "mysql -uroot -e 'GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';'"
+echo "mysql -uroot -e 'ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASS}';'"
 
-cat init.sql
 
 rc-service mariadb stop
 
