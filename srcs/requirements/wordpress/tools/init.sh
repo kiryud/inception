d4f4dbb298cc62cccd72f954a62b9ea1d3456175
jijeong <jijeong@cx2r2s9.42seoul.kr>
2023-12-27 15:08:41 +0900

    asdf

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -7,32 +7,42 @@ echo "listen.owner = www-data" >> /etc/php81/php-fpm.d/www.conf
 echo "listen.group = www-data" >> /etc/php81/php-fpm.d/www.conf
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-cat << EOF > /var/www/html /wp-config.php
-	<?php
-	define( 'DB_NAME', '$MARIADB_DATABASE_NAME' );
-	define( 'DB_USER', '$MARIADB_USER_' );
-	define( 'DB_PASSWORD', '$MARIADB_PASS' );
-	define( 'DB_HOST', 'mariadb' );
-	define( 'DB_CHARSET', 'utf8' );
-	define( 'DB_COLLATE', '' );
-	$table_prefix = 'wp_';
+#cat << EOF > /var/www/html /wp-config.php
+#	<?php
+#	define( 'DB_NAME', '$MARIADB_DATABASE_NAME' );
+#	define( 'DB_USER', '$MARIADB_USER_' );
+#	define( 'DB_PASSWORD', '$MARIADB_PASS' );
+#	define( 'DB_HOST', 'mariadb' );
+#	define( 'DB_CHARSET', 'utf8' );
+#	define( 'DB_COLLATE', '' );
+#	$table_prefix = 'wp_';
+#
+#	define( 'WP_SITEURL', '$DOMAIN_NAME' );
+#EOF
 
-	define( 'WP_SITEURL', '$DOMAIN_NAME' );
-EOF
+	wp core config \
+	--dbname=TEMP_DATABASE_NAME_FOR_DEL \
+	--dbuser=TEMP_DATABASE_USER_NAME_FOR_DEL \
+	--dbpass=TEMP_DATABASE_USER_PW_FOR_DEL \
+	--dbhost=localhost \
+	--dbprefix=wp_
 
-#wp core install \
-#	--url=$DOMAIN_NAME \
-#	--title="$WORDPRESS_TITLE" \
-#	--admin_user=$WORDPRESS_ADMIN_USER \
-#	--admin_password=$WORDPRESS_ADMIN_PASS \
-#	--admin_email=$WORDPRESS_ADMIN_MAIL \
-#	--skip-email \
-#	--allow-root
+	wp core install \
+	--url=$DOMAIN_NAME \
+	--title="$WORDPRESS_TITLE" \
+	--admin_user=$WORDPRESS_ADMIN_USER \
+	--admin_password=$WORDPRESS_ADMIN_PASS \
+	--admin_email=$WORDPRESS_ADMIN_MAIL \
+	--skip-email \
+	--allow-root
 
-#wp user create \
-#	$WORDPRESS_USER \
-#	$WORDPRESS_MAIL \
-#	--role=author \
-#	--user_pass=$WORDPRESS_PASS \
-#	--allow-root
+	wp user create \
+	$WORDPRESS_USER \
+	$WORDPRESS_MAIL \
+	--role=author \
+	--user_pass=$WORDPRESS_PASS \
+	--allow-root
+
 fi
+
+/usr/sbin/php-fpm81 -F
