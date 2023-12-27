65476cbfa5854330a25679d8ac8fad5c549ea580
jijeong <jijeong@cx2r2s9.42seoul.kr>
2023-12-27 15:55:37 +0900

    asdf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,29 +1,5 @@
 #! /bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]
-then
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-	tmp=".tmp_init_db.sql"
-
-	if [ -f "$tmp" ]
-	then
-		return 1
-	fi
-
-cat << EOF > $tmp
-	USE mysql; 
-	FLUSH PRIVILEGES;
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ADMIN_PASS';
-	CREATE DATABASE $MARIADB_DATABASE_NAME;
-	CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASS';
-	GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER'@'%';
-	FLUSH PRIVILEGES;
-EOF
-
-	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
-	rm -f $tmp
-fi
 
 exec /usr/bin/mysqld --user=mysql --console
